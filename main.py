a277e6a6c8a795755c11e082d14ed9b5afdb49ba
Victor Simonin <vsimonin@lrde.epita.fr>
2022-03-16 00:08:24 +0100

    [Add] Selection Function

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from collections import namedtuple
 from random import choices
+from typing import Callable
 import numpy as np
 
-genome = []
+Genome = []
 population = []
+FitnessFunc = Callable[[Genome], int]
 Thing = namedtuple('Thing', ['name', 'value', 'weight'])
+
+things = [
+    Thing('Laptop', 500, 2200),
+    Thing('Headphones', 150, 160),
+    Thing('Coffee Mug', 60, 350),
+    Thing('Notepad', 40, 333),
+    Thing('Water Bottle', 30, 192)
+]
+
+more_things = [
+    Thing('Mints', 5, 25),
+    Thing('Socks', 10, 38),
+    Thing('Tissues', 15, 80),
+    Thing('Phone', 500, 200),
+    Thing('Baseball Cap', 100, 70)
+]
 
 def generate_genome(length: int):
     return choices([0, 1], k=length)
@@ -19,7 +37,7 @@ def fitness(genome, things, weight_limit):
     weight = 0
     value = 0
 
-    for i, things in enumerate(things):
+    for i, thing in enumerate(things):
         if genome[i] == 1:
             weight += thing.weight
             value += thing.value
@@ -28,3 +46,11 @@ def fitness(genome, things, weight_limit):
                 return 0
     
     return value
+
+def selection_pair(population, fitness_func):
+    return choices(
+        population=population,
+        weights=[fitness_func(genome) for genome in population],
+        k=2
+    )
+
